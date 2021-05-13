4bf24c60b0646798f02814c3fe92f9d3ae94e38b
phillipmjordan <github@phillipjordan.com>
2021-05-12 23:43:08 -0700

    Fix preview

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -4,26 +4,19 @@ import { withPreviewResolver } from 'gatsby-source-prismic'
 import { graphql, useStaticQuery } from 'gatsby'
 import linkResolver from '../utils/linkResolver'
 
+import { Layout } from '../components/Layout'
+
 const PreviewPage = ({ isPreview }) => {
   if (isPreview === false) return 'Not a preview!'
 
-  return (<p>Loading</p>)
+  return (
+    <Layout>
+      <p>Loading</p>
+    </Layout>
+  )
 }
 
-const Preview = (props) => {
-  const data = useStaticQuery(graphql`query {
-    sitePlugin(name: {eq: "gatsby-source-prismic"}) {
-      pluginOptions {
-        repositoryName
-      }
-    }
-  }
-  `)
-  const { repositoryName } = data.sitePlugin.pluginOptions
-  return withPreviewResolver(PreviewPage, {
-    repositoryName,
-    linkResolver: () => linkResolver,
-  })(props)
-}
-
-export default Preview;
+export default withPreviewResolver(PreviewPage, {
+  repositoryName: process.env.GATSBY_PRISMIC_REPOSITORY_NAME,
+  linkResolver,
+})
